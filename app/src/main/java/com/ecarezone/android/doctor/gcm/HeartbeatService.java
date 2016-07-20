042fb8e5bfb01694a4db41d625e79bb98380f42a
Satish Megadri <satish.megadri@lnttechservices.com>
2016-07-20 12:55:17 +0530

    Status change at runtime using apache network APIs

--- app/src/main/java/com/ecarezone/android/doctor/gcm/HeartbeatService.java
+++ app/src/main/java/com/ecarezone/android/doctor/gcm/HeartbeatService.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import com.ecarezone.android.doctor.DoctorApplication;
 import com.ecarezone.android.doctor.config.Constants;
 import com.ecarezone.android.doctor.config.LoginInfo;
+import com.ecarezone.android.doctor.model.UserProfile;
 import com.ecarezone.android.doctor.model.database.ProfileDbApi;
 import com.ecarezone.android.doctor.model.rest.base.BaseResponse;
 import com.ecarezone.android.doctor.model.rest.base.ChangeStatusRequest;
@@ -14,6 +15,21 @@ import com.ecarezone.android.doctor.service.RoboEcareSpiceServices;
 import com.octo.android.robospice.SpiceManager;
 import com.octo.android.robospice.persistence.exception.SpiceException;
 import com.octo.android.robospice.request.listener.RequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import ch.boye.httpclientandroidlib.HttpEntity;
+import ch.boye.httpclientandroidlib.HttpResponse;
+import ch.boye.httpclientandroidlib.client.ClientProtocolException;
+import ch.boye.httpclientandroidlib.client.HttpClient;
+import ch.boye.httpclientandroidlib.client.methods.HttpPost;
+import ch.boye.httpclientandroidlib.entity.ByteArrayEntity;
+import ch.boye.httpclientandroidlib.impl.client.HttpClientBuilder;
 
 /**
  * Created by Umesh on 27-06-2016.
@@ -47,14 +63,11 @@ public class HeartbeatService extends IntentService {
                 } else {
                     status = Constants.ONLINE;
                 }
-                ProfileDbApi profileDbApi = ProfileDbApi.getInstance(getApplicationContext());
-                String name = profileDbApi.getMyProfile().name;
+
                 if (doctorApplication.getLastAvailabilityStaus() != status) {
 
-                    ChangeStatusRequest request = new ChangeStatusRequest(
-                            LoginInfo.userName, LoginInfo.hashedPassword,
-                            name, Constants.USER_ROLE, status, Constants.deviceUnique);
-                    getSpiceManager().execute(request, new ChangeStatusRequestListener());
+                    ChangeStatusRequest changeStatusService = new ChangeStatusRequest(status);
+                    changeStatusService.startHttpRequest();
                 }
             } else {
                 doctorApplication.getNameValuePair().put(Constants.STATUS_CHANGE, false);
@@ -65,30 +78,75 @@ public class HeartbeatService extends IntentService {
         }
     }
 
-    public final class ChangeStatusRequestListener implements RequestListener<String> {
+    private class ChangeStatusRequest {
 
-        private String TAG = "ChangeStatusRequestListener";
+// {"email":"uapatient1@gmail.com", "password":"wkkdl/bt34SeumhQNMNlzQ==",
+// "name":"name", "role": "0","status":"0","deviceUnique":"b5d4c425-a305-4363-8d6a-f3fb65635abf"}
 
-        @Override
-        public void onRequestFailure(SpiceException spiceException) {
-//            progressDialog.dismiss();
+        private String requestBody;
+        public ChangeStatusRequest(int status) {
+
+            ProfileDbApi profileDbApi = ProfileDbApi.getInstance(getApplicationContext());
+            UserProfile userProfile = profileDbApi.getMyProfile();
+
+            JSONObject jsonObj = new JSONObject();
+            try {
+                jsonObj.put("email", LoginInfo.userName);
+                jsonObj.put("password", LoginInfo.hashedPassword);
+                jsonObj.put("name", userProfile.name);
+                jsonObj.put("role", Constants.USER_ROLE);
+                jsonObj.put("status", status);
+                jsonObj.put("deviceUnique", Constants.deviceUnique);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            requestBody = jsonObj.toString();
         }
 
-        @Override
-        public void onRequestSuccess(final String baseResponse) {
-            Log.d(TAG, "statuschange " + "changed");
+        public void startHttpRequest() {
 
-//            DoctorApplication.lastAvailablityStaus = status ;
+            String response = null;
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpPost httpPost = new HttpPost("http://188.166.55.204:8080/ECZ/notification/pushstatus/" + LoginInfo.userId);
+            // Set content type
+            httpPost.setHeader("Content-Type", "application/json");
+
+            if(requestBody != null) {
+                //Post Data
+                HttpEntity entity = new ByteArrayEntity(requestBody.getBytes());
+                httpPost.setEntity(entity);
+            }
+
+            //making POST request.
+            try {
+                HttpResponse responseHttp = httpClient.execute(httpPost);
+                if (responseHttp.getStatusLine().getStatusCode() == 200) {
+                    StringBuilder sb = new StringBuilder();
+
+                    BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(
+                                    responseHttp.getEntity().getContent()));
+                    String resp = null;
+                    while ((resp = reader.readLine()) != null) {
+                        sb.append(resp);
+                    }
+                    response = sb.toString();
+                }
+                // write response to log
+                Log.d("Http Post Response:", responseHttp.toString());
+            } catch (ClientProtocolException e) {
+                // Log exception
+                e.printStackTrace();
+            } catch (IOException e) {
+                // Log exception
+                e.printStackTrace();
+            }
+
+            if (response != null && response.equalsIgnoreCase("Notification Sent")) {
+                Log.i("HeartbeatService", response);
+            } else {
+                Log.i("HeartbeatService", "Notification Not Sent: " + response);
+            }
         }
     }
-
-    private SpiceManager spiceManager = new SpiceManager(RoboEcareSpiceServices.class);
-
-    public SpiceManager getSpiceManager() {
-        if (!spiceManager.isStarted()) {
-            spiceManager.start(this);
-        }
-        return spiceManager;
-    }
-
 }

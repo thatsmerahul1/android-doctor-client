fbe57c41c3a104a2da806f3ddc1151a7fe9aaf74
naina <naina.jain@lntTechservices.com>
2016-09-10 18:04:38 +0530

    Alarm Receiver. UAt bug fixes

--- app/src/main/java/com/ecarezone/android/doctor/utils/AppointmentAlarmReceiver.java
+++ app/src/main/java/com/ecarezone/android/doctor/utils/AppointmentAlarmReceiver.java
@@ -2,12 +2,14 @@ package com.ecarezone.android.doctor.utils;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 
+import com.ecarezone.android.doctor.MyPatientActivity;
 import com.ecarezone.android.doctor.R;
 import com.ecarezone.android.doctor.model.database.PatientProfileDbApi;
 import com.ecarezone.android.doctor.model.rest.Patient;
@@ -36,7 +38,8 @@ public class AppointmentAlarmReceiver extends BroadcastReceiver {
 
             PatientProfileDbApi patientProfileDbApi = PatientProfileDbApi.getInstance(context);
             Patient patient = patientProfileDbApi.getProfile(String.valueOf(intent.getIntExtra("patId", 0)));
-
+            PendingIntent contentIntent =
+                    PendingIntent.getActivity(context, 0, new Intent(context, MyPatientActivity.class), 0);
             NotificationCompat.Builder mNotifyBuilder = new NotificationCompat.Builder(context)
                     .setContentTitle("Appointment with " + patient.name)
                     .setContentText("You have an "+appointment_type+" appointment with "+patient.name)
@@ -45,6 +48,7 @@ public class AppointmentAlarmReceiver extends BroadcastReceiver {
                     .setColor(Color.BLUE)
                     .setAutoCancel(true);
 
+            mNotifyBuilder.setContentIntent(contentIntent);
             Notification notification = mNotifyBuilder.build();
             notification.flags |= Notification.FLAG_AUTO_CANCEL;
             NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
